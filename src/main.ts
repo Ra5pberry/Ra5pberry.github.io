fc1d82569f39de73db0262b05d3dd9c41bbf1d80
Sophia Nara <rosu.corb@gmail.com>
2022-07-23 01:40:29 +0900

    update main.ts. removed vue-axios

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-
-VueAxios.arguments(VueAxios, axios);
 
 createApp(App).mount("#app");
