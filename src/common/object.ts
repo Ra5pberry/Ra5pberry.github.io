c7815eca6f78f5e7d1700d22688c164efc58d86a
Sophia Nara <rosu.corb@gmail.com>
2022-07-28 09:57:46 +0900

    update some interface

--- src/common/object.ts
+++ src/common/object.ts
@@ -1,8 +1,6 @@
 export interface PrefData {
-  result: {
-    prefCode: number;
-    prefName: string;
-  };
+  prefCode: number;
+  prefName: string;
 }
 
 export interface PopulationData {
@@ -16,4 +14,16 @@ export interface PopulationData {
 export interface GraphData {
   value: number;
   year: number;
+}
+
+export interface ChartDataSet {
+  label: string;
+  data: number[];
+}
+
+export class Datasets implements ChartDataSet {
+  constructor(public label: string, public data: number[]) {
+    this.label = label;
+    this.data = data;
+  }
 }
