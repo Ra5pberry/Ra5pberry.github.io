d31dea1a031780444531db9ec0893f0e24278841
Sophia Nara <rosu.corb@gmail.com>
2022-07-28 17:48:06 +0900

    update unit test (WIP)

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,30 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mount, shallowMount } from "@vue/test-utils";
+import App from "@/App.vue";
+import PopulationGraph from "@/components/PopulationGraph.vue";
+import PrefSelect from "@/components/PrefSelect.vue";
+import HeaderComponent from "@/components/common/HeaderComponent.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+it("test App component", () => {
+  const wrapper = shallowMount(App);
+  console.log(wrapper.html());
+});
+
+describe("Prefecture Select", () => {
+  let wrapper: any;
+  beforeEach(async () => {
+    wrapper = shallowMount(PrefSelect);
   });
+  // it("test PrefSelect component", () => {
+  //   expect(wrapper.vm.)
+  // });
+});
+
+it("test PopulationGraph component", () => {
+  const wrapper = shallowMount(PopulationGraph);
+  console.log(wrapper.html());
+});
+
+it("test Header component", () => {
+  const wrapper = shallowMount(HeaderComponent);
+  expect(wrapper.text()).toMatch("都道府県別人口推移グラフ");
 });
